4758942de188e0bdf39b1904e8e57f66c11d421a
Nobrain11 <chainpulseink@gmail.com>
2026-08-24 09:03:31 +0100

    Create db.ts

---
+++ src/db.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+export const prisma =
+  new PrismaClient();
+
+export async function connectDatabase() {
+  await prisma.$connect();
+
+  console.log(
+    "🗄️ PostgreSQL connected"
+  );
+}
+
+export async function disconnectDatabase() {
+  await prisma.$disconnect();
+}
